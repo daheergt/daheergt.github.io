db47d3be887aea899610a6944cc397b653be249a
daheergt <kdaheer9@gmail.com>
2025-03-20 22:34:22 +0400

    updae

--- Script.js
+++ Script.js
@@ -3,8 +3,7 @@ var paragraph = document.getElementById("Work");
 fetch("https://raw.githubusercontent.com/daheergt/daheergt.github.io/refs/heads/main/work.txt")
     .then((res) => res.text())
     .then((text) => {
-
-        paragraph.textContent = text + "";
+        paragraph.textContent += text + "";
     })
 
 function Show() {

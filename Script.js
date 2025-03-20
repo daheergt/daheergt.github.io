637d539be5c267c2d187285b90f53a86446aa87b
daheergt <kdaheer9@gmail.com>
2025-03-20 22:25:19 +0400

    links

--- Script.js
+++ Script.js
@@ -1,7 +1,25 @@
 var paragraph = document.getElementById("Work");
 
+function replaceURLs(content) {
+    if (!content) return;
+    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    return content.replace(urlRegex, function (url) {
+        var hyperlink = url;
+        if (!hyperlink.match('^https?:\/\/')) { hyperlink = 'http://' + hyperlink; }
+        return '<a href="' + hyperlink + '" target="_blank" rel="noopener noreferrer">' + url + '</a>'
+    });
+}
+
 fetch("https://raw.githubusercontent.com/daheergt/daheergt.github.io/refs/heads/main/work.txt")
     .then((res) => res.text())
     .then((text) => {
-        paragraph.textContent += text + "";
+
+        paragraph.textContent += replaceURLs(text) + "";
     })
+
+function Show() {
+    document.getElementById("JobWind").style.transform = "translate(-50%, -50%) scale(1)";
+}
+function Hide() {
+    document.getElementById("JobWind").style.transform = "translate(-50%, 50%) scale(0)";
+}

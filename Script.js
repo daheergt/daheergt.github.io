9d7a50c4d121923ea1d09319f9529a5ade4d0a35
daheergt <kdaheer9@gmail.com>
2025-03-20 22:31:37 +0400

    Replace

--- Script.js
+++ Script.js
@@ -1,20 +1,10 @@
 var paragraph = document.getElementById("Work");
 
-function replaceURLs(content) {
-    if (!content) return;
-    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
-    return content.replace(urlRegex, function (url) {
-        var hyperlink = url;
-        if (!hyperlink.match('^https?:\/\/')) { hyperlink = 'http://' + hyperlink; }
-        return '<a href="' + hyperlink + '" target="_blank" rel="noopener noreferrer">' + url + '</a>'
-    });
-}
-
 fetch("https://raw.githubusercontent.com/daheergt/daheergt.github.io/refs/heads/main/work.txt")
     .then((res) => res.text())
     .then((text) => {
 
-        paragraph.textContent += replaceURLs(text) + "";
+        paragraph.textContent = text + "";
     })
 
 function Show() {
